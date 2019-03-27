ebc1c5bd16d871fcec3642421c65eb466f969a0f
hemant.gogoi <hemant.gogoi@credr.com>
2019-03-27 21:44:38 +0530

    fix() : changes in the design

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './style.scss';
 import Header from './Components/Header/Header.js'
 import Footer from './Components/Footer/Footer.js'
-import LoanComponent from './Components/LoanComponent/LoanComponent.js'
+import GitSearchCard from './Components/GitSearchCard/GitSearchCard.js'
 import { Provider } from "react-redux";
 import Store from './Store.js'
 import "../node_modules/font-awesome/css/font-awesome.min.css";
@@ -14,7 +14,7 @@ class App extends Component {
         <div>
           <Header></Header>
           <div className="wrapper">
-            <LoanComponent></LoanComponent>
+            <GitSearchCard></GitSearchCard>
           </div>
           <Footer></Footer>
         </div>

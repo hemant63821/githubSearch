593d86a62a3ac23ec2317d3e6bd63ed4374892a1
hemant.gogoi <hemant.gogoi@credr.com>
2019-03-28 00:25:15 +0530

    feat() : created projects containers

---
+++ src/Container/ProjectSearch-container.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+class ProjectContainer extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div className="row">
+                <div className="card">
+                    <div className="card-body">
+                        <h1>Projects</h1>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+
+
+export default ProjectContainer;

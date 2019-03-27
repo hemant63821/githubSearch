4e21c84430ee534a0c1932ba7ac81260591d5e9f
hemant.gogoi <hemant.gogoi@credr.com>
2019-03-28 02:05:46 +0530

    fix() : implemented bike card with pagination logic

--- src/Components/GitSearchCard/GitSearchCard.js
+++ src/Components/GitSearchCard/GitSearchCard.js
@@ -5,20 +5,20 @@ class GitSearchCard extends Component {
     render() {
         const { projectTitle, description } = this.props
         return (
-            <div className="row">
-                <div className="col-sm-4 col-lg-4">
-                    <div className="card">
-                        <div className="card-body text-center ripple">
-                            <h5 className="card-title">
-                                {projectTitle}
-                            </h5>
-                            <p className="card-text">
-                                {description}
-                            </p>
-                        </div>
+
+            <div className="col-sm-4 col-lg-4 mt-2">
+                <div className="card">
+                    <div className="card-body text-center ripple">
+                        <h5 className="card-title">
+                            {projectTitle}
+                        </h5>
+                        <p className="card-text">
+                            {description}
+                        </p>
                     </div>
                 </div>
             </div>
+
         )
     }
 }
--- src/Container/ProjectSearch-container.js
+++ src/Container/ProjectSearch-container.js
@@ -10,46 +10,68 @@ class ProjectContainer extends Component {
     constructor(props) {
         super(props)
         this.state = {
+            currentPage: 0
         }
         this.pageCount = 0
+        this.results = []
     }
 
 
     handlePageClick = (pageNum) => {
-
+        console.log('check page number', pageNum)
+        this.setState({
+            currentPage: (pageNum.selected)
+        })
     }
 
     render() {
-        if (this.props.totalCount != null) {
-            console.log('check', this.props.totalCount, this.props.totalCount.length)
-            this.productCount = Number(this.props.totalCount)
-            if (this.productCount == 0) {
-                this.pageCount = 0
+        if (this.props.searchResults != null) {
+            this.results = this.props.searchResults
+        }
+        console.log('check projects length', this.results.length)
+
+        const indexOfLastTodo = (this.state.currentPage + 1) * 12;
+        const indexOfFirstTodo = indexOfLastTodo - 12;
+        const projects = this.results.slice(indexOfFirstTodo, indexOfLastTodo);
+
+        console.log('check projects', projects.length)
+
+        this.productCount = this.results.length
+        if (this.productCount == 0) {
+            this.pageCount = 0
+        }
+        else {
+            if (this.productCount < 12) {
+                this.pageCount = 1
             }
             else {
-                if (this.productCount < 12) {
-                    this.pageCount = 1
+                if ((this.productCount % 12) == 0) {
+                    this.pageCount = Math.trunc(this.productCount / 12)
                 }
                 else {
-                    if ((this.productCount % 12) == 0) {
-                        this.pageCount = Math.trunc(this.productCount / 12)
-                    }
-                    else {
-                        this.pageCount = Math.trunc(this.productCount / 12) + 1
-                    }
+                    this.pageCount = Math.trunc(this.productCount / 12) + 1
                 }
             }
         }
+
         return (
             <div className="row">
                 <div className="card">
                     <div className="card-body">
                         <h1>Projects</h1>
-                        <GitSearchCard></GitSearchCard>
+                        <div className="row">
+                            {
+                                projects.length ? projects.map((value, key) => {
+                                    return (
+                                        <GitSearchCard key={key} projectTitle={value.full_name} description={value.description}></GitSearchCard>
+                                    )
+                                }) : null
+                            }
+                        </div>
                     </div>
 
                     {
-                        (this.pageCount != 0) ?
+                        (this.props.totalCount != 0) ?
 
                             (
                                 <div className="paginated">
@@ -65,7 +87,7 @@ class ProjectContainer extends Component {
                                         pageCount={this.pageCount}
                                         marginPagesDisplayed={2}
                                         pageRangeDisplayed={3}
-                                        forcePage={this.currentPage}
+                                        forcePage={this.state.currentPage}
                                         onPageChange={(page) => this.handlePageClick(page)}
                                         containerClassName={'pagination pages'}
                                         activeClassName={'active'}

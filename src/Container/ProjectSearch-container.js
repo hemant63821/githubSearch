8a11616ea73e19ae3de83fc2ea4113bb75f37fe5
hemant.gogoi <hemant.gogoi@credr.com>
2019-03-28 01:21:21 +0530

    feat() :pagination installed and integrated

--- src/Container/ProjectSearch-container.js
+++ src/Container/ProjectSearch-container.js
@@ -2,19 +2,43 @@ import React, { Component } from "react";
 import GitSearchCard from '../Components/GitSearchCard/GitSearchCard.js'
 import { connect } from 'react-redux'
 import { fetchSearchResults } from '../Actions/SearchAction'
+import './ProjectSearch-container.scss'
+import ReactPaginate from 'react-paginate'
 
 class ProjectContainer extends Component {
 
     constructor(props) {
         super(props)
         this.state = {
-
         }
+        this.pageCount = 0
+    }
+
+
+    handlePageClick = (pageNum) => {
+
     }
 
     render() {
         if (this.props.totalCount != null) {
-            console.log('check', this.props.totalCount)
+            console.log('check', this.props.totalCount, this.props.totalCount.length)
+            this.productCount = Number(this.props.totalCount)
+            if (this.productCount == 0) {
+                this.pageCount = 0
+            }
+            else {
+                if (this.productCount < 12) {
+                    this.pageCount = 1
+                }
+                else {
+                    if ((this.productCount % 12) == 0) {
+                        this.pageCount = Math.trunc(this.productCount / 12)
+                    }
+                    else {
+                        this.pageCount = Math.trunc(this.productCount / 12) + 1
+                    }
+                }
+            }
         }
         return (
             <div className="row">
@@ -23,7 +47,33 @@ class ProjectContainer extends Component {
                         <h1>Projects</h1>
                         <GitSearchCard></GitSearchCard>
                     </div>
+
+                    {
+                        (this.pageCount != 0) ?
+
+                            (
+                                <div className="paginated">
+                                    <ReactPaginate
+                                        previousLinkClassName={'page-link'}
+                                        previousLabel={'previous'}
+                                        pageClassName={'page-item'}
+                                        pageLinkClassName={'page-link'}
+                                        nextLinkClassName={'page-link'}
+                                        nextLabel={'next'}
+                                        breakLabel={'...'}
+                                        breakClassName={'break-me'}
+                                        pageCount={this.pageCount}
+                                        marginPagesDisplayed={2}
+                                        pageRangeDisplayed={3}
+                                        forcePage={this.currentPage}
+                                        onPageChange={(page) => this.handlePageClick(page)}
+                                        containerClassName={'pagination pages'}
+                                        activeClassName={'active'}
+                                    ></ReactPaginate>
+                                </div>) : null
+                    }
                 </div>
+
             </div>
         )
     }
